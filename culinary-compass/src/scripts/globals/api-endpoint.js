1b0cea9ab05bf0cc0eecf62c662c68d7c6eef344
LaelaAnggraeni08 <91456327+LaelaAnggraeni08@users.noreply.github.com>
2023-11-16 22:50:50 +0700

    Delete culinary-compass directory

--- culinary-compass/src/scripts/globals/api-endpoint.js
+++
@@ -1,9 +0,0 @@
-import CONFIG from './config';
-
-const API_ENDPOINT = {
-  HOME: `${CONFIG.BASE_URL}list`,
-  FAVORITE: `${CONFIG.BASE_URL}review`,
-  DETAIL: (id) => `${CONFIG.BASE_URL}detail/${id}`,
-};
-
-export default API_ENDPOINT;
